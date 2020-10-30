23c0f0bdbaebbbf8a8b1fe9b447b0863cfe84df7
souley <s.madougou@esciencecenter.nl>
2020-10-29 23:52:37 -0400

    Final checks and cleanup.

--- LOFAR_IEE_pipeline/__init__.py
+++ LOFAR_IEE_pipeline/__init__.py
@@ -29,52 +29,6 @@ def give_argument_names(required=False):
 
 
 def run_pipeline(observation, observation2, **kargs):
-    print(kargs)
-    print("###Observation: ", observation)
-    print("###Observation2: ", observation2)
-    # Request for staging data from LOFAR LTA
-    webhook = "http://localhost:8000/sessions"
-    #    srmuris = ["srm://srm.grid.sara.nl:8443/pnfs/grid.sara.nl/data/lofar/ops/projects/lofarschool/246403/L246403_SAP000_SB000_uv.MS_7d4aa18f.tar"]
-    tarfiles = observation.split("|")
-    srmuris = [] #tarfiles[:20] # testing
-    for tfile in tarfiles:
-        if re.search('SB0[0|1]', tfile):
-            srmuris.append(tfile)
-
-    url = '/stage'
-    headers = {
-        'Content-Type': 'application/json',
-    }
-    
-    data = {
-        "id": "staging",
-        "cmd": {
-            "type": "stage",
-            "subtype": "lofar",
-            "src": {
-                "type": "srm",
-                "paths": srmuris
-            },
-            "credentials": {}
-        },
-        #        "webhook": {"method": "post", "url": webhook, "headers": {}},
-        "options": {},
-    }
-    
-    #    print(kargs)
-    for kw in kargs:
-        if kw == "staging":
-            url = kargs[kw]["url"] + url
-            data["cmd"]["credentials"]["lofarUsername"] = kargs[kw]["login"]
-            data["cmd"]["credentials"]["lofarPassword"] = kargs[kw]["pwd"]
-    reqData = json.dumps(data)
-    print("===REQ URL=", url)
-    print("===REQ DATA=", reqData)
-#    res = requests.post(url, headers=headers, data=reqData)
-#    print(res)
-#    res_data = json.loads(res.content.decode("utf8"))
-#    print("Your staging request ID is ", str(res_data["requestId"]))
-    res = ""
-    return res
+    return ""
 
 
